4bc8784e423f423f28b69001f967c8b0103f2478
Sumer Mixon <gitSumer@ximm.us>
2014-01-23 15:37:18 -0500

    Sass-ifed the normalize reset

--- config.rb
+++ config.rb
@@ -1,5 +1,3 @@
-# Require any additional compass plugins here.
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
@@ -7,19 +5,17 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# Sytax:
 preferred_syntax = :scss
+
+# Use comment to pick environment
+
+# PRODUCTION:
+#environment  = :production
+#output_style = :compressed
+
+# DEVELOPMENT:
+environment  = :development
+output_style = :expanded
+
+# more info on this file here: http://compass-style.org/help/tutorials/configuration-reference/
